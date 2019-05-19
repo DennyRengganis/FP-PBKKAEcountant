3340f44eb3f48c851570ed32c147d764c0a14797
DennyRengganis <drdennyr1245@gmail.com>
2019-05-19 20:39:05 +0700

    Pemasukan Pengeluaran total

---
+++ src/com/pbkkaddm/entity/User.java
@@ -0,0 +1,66 @@
+package com.pbkkaddm.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="name")
+	private String email;
+	
+	public User() {
+		
+	}
+	
+	public User(String username,String password,String email) {
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setPassword(password);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", email=" + email + "]";
+	}
+}
